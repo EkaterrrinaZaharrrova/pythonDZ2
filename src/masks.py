fbacc857a14e1a3f192a72ef513137cc4e8410ee
Екатерина Захарова <k6orodaenko@yandex.ru>
2025-02-06 16:31:34 +0300

    start

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(number_card: str) -> str:
+    """принимает на вход номер карты в виде числа и возвращает маску номера"""
+    if len(number_card) == 16:
+        string = number_card[:4] + " " + number_card[4:7] + "** **** " + number_card[-4:]
+        return string
+    else:
+        return "Неверный номер карты"
+
+
+def get_mask_account(account: str) -> str:
+    """принимает на вход номер счета в виде числа и возвращает маску номера"""
+    if len(account) == 20:
+        string = "**" + account[-4:]
+        return string
+    else:
+        return "Неверный номер счета"
