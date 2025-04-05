4c3522418ffdf8c16296fd8c133acafa93de26a0
Екатерина Захарова <k6orodaenko@yandex.ru>
2025-04-05 12:06:20 +0300

    add modul utils.py and external_api.py

---
+++ src/external_api.py
@@ -0,0 +1,21 @@
+import os
+from typing import Any
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+API_KEY = os.getenv("API_KEY")
+
+
+def get_convert_currency(amount: float, currency: str) -> Any:
+    """Принимает сумму транзакции и её валюту.
+    Возвращает сумму, по актуальному курсу в указанной валюте."""
+
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+
+    headers = {"apikey": API_KEY}
+
+    response = requests.get(url, headers=headers)
+
+    return response.json()["result"]
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,9 @@
 import json
+import logging
+from typing import Any
+
+from src.external_api import get_convert_currency
+
 
 def read_json_file(filename: str = "") -> list:
     """Функция чтения JSON-файла"""
@@ -7,3 +12,24 @@ def read_json_file(filename: str = "") -> list:
         return list(data_json)
     except Exception:
         return []
+
+
+def transactions(operations: list[dict]) -> Any:
+    """Возвращает сумму транзакции из списка.
+    Транзакции в другой валюте обрабатываются через API-запрос."""
+
+    try:
+        for elem in operations:
+            if not len(elem):
+                continue
+
+            if elem["operationAmount"]["currency"]["code"] == "RUB":
+                return float(elem["operationAmount"]["amount"])
+            else:
+                return get_convert_currency(
+                    elem["operationAmount"]["amount"], elem["operationAmount"]["currency"]["code"]
+                )
+
+    except Exception as er:
+        logging.error(f" Ошибка {er}")
+
